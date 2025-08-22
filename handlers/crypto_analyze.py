691204daf76c3ab52932dd1ebe07fccde8cb02b9
dim pop <popdim21@gmail.com>
2025-08-22 11:43:38 +0300

    flamin hot

--- handlers/crypto_analyze.py
+++ handlers/crypto_analyze.py
@@ -1,11 +1,10 @@
 from aiogram import Router, F, Bot
 from aiogram.types import Message
 from utils.coingeeko_service import get_current_price, get_daily_summary, get_historical_data
-from utils.indicators import get_rsi, get_ema, simple_signal
+from utils.indicators import get_rsi, get_ema, simple_signal, get_macd, macd_signal
 from utils.text_template import make_ai_prompt
 from utils.openai_service import get_ai_prediction
 from keyboards import analyze_keyboard, price_keyboard, ai_keyboard
-
 
 router = Router()
 
@@ -26,7 +25,7 @@ async def price(message: Message):
 
 
 @router.message(F.text.startswith('/analyze'))
-async def analyze(message: Message,bot:Bot):
+async def analyze(message: Message, bot: Bot):
     parts = message.text.strip().split()
     if len(parts) < 2:
         await message.reply("Укажите ID монеты /analyze bitcoin", reply_markup=analyze_keyboard.keyboard)
@@ -40,6 +39,13 @@ async def analyze(message: Message,bot:Bot):
     current_rsi = get_rsi(df, period=30)
     current_ema = get_ema(df, period=30)
     signal_rsi = simple_signal(rsi=current_rsi)
+    macd_data = get_macd(df, fast_period=14, slow_period=30, signal_period=9)
+    if len(df) > 1:
+        prev_macd_data = get_macd(df.iloc[:-1], fast_period=14, slow_period=30, signal_period=9)
+        macd_sig = macd_signal(macd_data["macd_line"], macd_data["signal_line"], macd_data["histogram"],
+                               prev_macd_data["histogram"])
+    else:
+        macd_sig = macd_signal(macd_data["macd_line"], macd_data["signal_line"], macd_data["histogram"])
     text = f"""💸{coin_id.capitalize()} за последние 24 часа:
     цена: *{summary["current_price"]:.2f} USD*
     минимальная цена: *{summary["min_price"]:.2f} USD*
@@ -47,7 +53,12 @@ async def analyze(message: Message,bot:Bot):
     объем: *{summary["total_volume"]:.2f}*
     RSI=*{current_rsi}*
     рекомендация по RSI:*{signal_rsi}*
-    EMA=*{current_ema}*"""
+    EMA=*{current_ema}*
+    MACD=*{macd_data["macd_line"]}*
+    сигнал по MACD:*{macd_data["signal_line"]}*
+    рекомендация по MACD:*{macd_sig}*
+    гистограма:*{macd_data["histogram"]}*
+"""
     await message.reply(text, parse_mode="Markdown")
     # await bot.send_chat_action(chat_id=message.chat.id, action="typing")
     # prompt = make_ai_prompt(coin_id, summary, current_rsi, current_ema, signal_rsi)
@@ -55,9 +66,8 @@ async def analyze(message: Message,bot:Bot):
     # await message.reply(answer, parse_mode="Markdown")
 
 
-
 @router.message(F.text.startswith('/ai'))
-async def ai(message: Message,bot:Bot):
+async def ai(message: Message, bot: Bot):
     parts = message.text.strip().split()
     if len(parts) < 2:
         await message.reply("Укажите ID монеты", reply_markup=ai_keyboard.keyboard)
